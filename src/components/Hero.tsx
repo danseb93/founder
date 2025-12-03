f27ef6b103967d07a9adbfbcfc3191341254dab6
Daniel Rincon <daniel.seb93@gmail.com>
2025-12-03 18:45:35 -0500

    fix: remove unused useState import from Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from './Button';
 import heroImage from '../assets/heroImage.png';
